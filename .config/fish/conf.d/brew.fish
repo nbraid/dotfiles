6b4052fa36d77d3c0e2fa86ea256ade6ba861a05
Nathan Braid <nbraid@gmail.com>
2022-09-27 17:35:11 -0700

    Adding yadm to brewfile, adding user gitconfig

--- .config/fish/conf.d/brew.fish
+++ .config/fish/conf.d/brew.fish
@@ -1,4 +1,8 @@
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -d /opt/homebrew && eval "$(/opt/homebrew/bin/brew shellenv)"
+
+set HOMEBREW_BUNDLE_FILE $HOME/Brewfile
 
 # ASDF
 source (brew --prefix asdf)/asdf.fish
