f7cd6dea5edea7d3be2c9c0599ed2b1ba99e3ecd
Nathan Braid <nbraid@gmail.com>
2026-05-18 16:08:47 -0700

    Apply default-rgb theme and add ~/.local/bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,11 @@ if status is-interactive
     # Disable annoying greeting message
     set -U fish_greeting
 
+    fish_config theme choose default-rgb
+
 end
+
+fish_add_path -g ~/.local/bin
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
